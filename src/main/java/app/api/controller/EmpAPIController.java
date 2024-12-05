f128a53c81467c0b2688fd5f89f0af0444ff3977
syjoy1993 <syjoy1993@gmail.com>
2024-12-05 22:05:52 +0900

    EmpAPIController update, delete

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -1,15 +1,73 @@
 package app.api.controller;
 
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import app.dto.EmpDto;
+import app.entity.Dept;
+import app.entity.Emp;
+import app.repository.DeptRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 import app.repository.EmpRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
 public class EmpAPIController {
 
 	private final EmpRepository empRepository;
+	private final DeptRepository deptRepository;
+
+	@PutMapping("/api/emp/{empno}")
+	public EmpDto updateEmp(@RequestBody EmpDto empDto ,
+						 @PathVariable Integer empno) {
+
+		Emp emp = empRepository.findById(empno).orElseThrow(() -> new EntityNotFoundException("정보가 존재하지 않습니다."));
+		Dept dept = deptRepository.findById(empDto.getDeptno()).orElseThrow(() -> new EntityNotFoundException("정보가 존재하지 않습니다."));
+
+		emp = Emp.builder()
+				.empno(empno)
+				.ename(empDto.getEname())
+				.job(empDto.getJob())
+				.mgr(empDto.getMgr())
+				.hiredate(empDto.getHiredate())
+				.sal(empDto.getSal())
+				.comm(empDto.getComm())
+				.dept(dept)
+				.build();
+
+		Emp updateEmp = empRepository.save(emp);
+
+
+		return EmpDto.builder()
+				.empno(updateEmp.getEmpno())
+				.ename(updateEmp.getEname())
+				.job(updateEmp.getJob())
+				.mgr(updateEmp.getMgr())
+				.hiredate(updateEmp.getHiredate())
+				.sal(updateEmp.getSal())
+				.comm(updateEmp.getComm())
+				.deptno(updateEmp.getDept().getDeptno())
+				.build();
+	}
+
+	@DeleteMapping("/api/emp/{empno}")
+	public EmpDto deleteDeptByEmp(@PathVariable Integer empno) {
+
+		Emp emp = empRepository.findById(empno).orElseThrow(() -> new EntityNotFoundException("정보가 존재하지 않습니다."));
+
+		empRepository.delete(emp);
+
+		return EmpDto.builder()
+				.empno(empno)
+				.ename(emp.getEname())
+				.job(emp.getJob())
+				.mgr(emp.getMgr())
+				.sal(emp.getSal())
+				.comm(emp.getComm())
+				.deptno(emp.getDept().getDeptno())
+				.build();
+	}
 
 }
---
+++ src/main/java/app/dto/EmpDto.java
@@ -0,0 +1,22 @@
+package app.dto;
+
+import app.entity.Dept;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class EmpDto {
+    private Integer empno;
+    private String ename;
+    private String job;
+    private Integer mgr;
+    private LocalDate hiredate;
+    private Double sal;
+    private Double comm;
+    private Integer deptno;
+}
