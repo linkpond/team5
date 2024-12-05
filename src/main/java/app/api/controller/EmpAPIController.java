9b3dc2eb3199cc8b084f4b6df37beea9ec3766e2
linkpond <linkpond0629@gmail.com>
2024-12-05 14:08:04 +0900

    add Emp

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -1,15 +1,28 @@
 package app.api.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.entity.Emp;
 import app.repository.EmpRepository;
+
 import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class EmpAPIController {
 
+//	private final EmpService empService;
 	private final EmpRepository empRepository;
 	
-	
+	@GetMapping("/emps")
+	public List<Emp> getEmps() {
+//		List<Emp> emps = empService.getEmps();
+		List<Emp> emps = empRepository.findAll();
+		return emps;
+	}
 }
--- src/main/java/app/repository/EmpRepository.java
+++ src/main/java/app/repository/EmpRepository.java
@@ -1,12 +1,17 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import app.entity.Emp;
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface EmpRepository extends JpaRepository<Emp, Integer>{
+	
+
 
 }
 
